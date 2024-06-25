10a8f5546701da76d6cfa7242d53ab8123e1a058
Valve04 <110170860+Valve04@users.noreply.github.com>
2024-06-25 22:06:40 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+# import streamlit as st
+
+# from streamlit_option_menu import option_menu
+
+# import about , account , home  , trending , your_post 
+
+# st.set_page_config(
+#         page_title="Pondering ",
+# )
+
+# class MultiApp:
+#     def __init__(self) -> None:
+#         self.apps = []
+
+#     def add_app(self,title,function):
+#         self.apps({
+#             "title":title,
+#             "function":function
+#         })
+
+#     def run():
+#         # app = st.sidebar(
+#         with st.sidebar:        
+#             app = option_menu(
+#                 menu_title='Pondering ',
+#                 options=['Home','Account','Trending','Your Posts','about'],
+#                 icons=['house-fill','person-circle','trophy-fill','chat-fill','info-circle-fill'],
+#                 menu_icon='chat-text-fill',
+#                 default_index=1,
+#                 styles={
+#                     "container": {"padding": "5!important","background-color":'black'},
+#         "icon": {"color": "white", "font-size": "23px"}, 
+#         "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
+#         "nav-link-selected": {"background-color": "#02ab21"},}
+                
+#                 )
+
+
+#         if app == "Home":
+#             home.app()
+#         if app == "Account":
+#             account.app()    
+#         if app == "Trending":
+#             trending.app()        
+#         if app == 'Your Posts':
+#             your_post.app()
+#         if app == 'about':
+#             about.app()    
+             
+          
+             
+#     run() 
+
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+import about, account, home, trending, your_post
+
+st.set_page_config(page_title="Pondering")
+
+class MultiApp:
+    def __init__(self) -> None:
+        self.apps = []
+
+    def add_app(self, title, function):
+        self.apps.append({"title": title, "function": function})
+
+    def run(self):
+        with st.sidebar:
+            app = option_menu(
+                menu_title='Pondering',
+                options=['Home', 'Account', 'Trending', 'Your Posts', 'About'],
+                icons=['house-fill', 'person-circle', 'trophy-fill', 'chat-fill', 'info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=0,
+                styles={
+                    "container": {"padding": "5!important", "background-color": 'black'},
+                    "icon": {"color": "white", "font-size": "23px"},
+                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "blue"},
+                    "nav-link-selected": {"background-color": "#02ab21"},
+                }
+            )
+
+        for app_dict in self.apps:
+            if app == app_dict['title']:
+                app_dict['function']()
+
+app = MultiApp()
+app.add_app("Home", home.app)
+app.add_app("Account", account.app)
+app.add_app("Trending", trending.app)
+app.add_app("Your Posts", your_post.app)
+app.add_app("About", about.app)
+
+app.run()
